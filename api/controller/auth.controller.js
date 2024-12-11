7d205286f1cdfb2fe4adf070af63556a6839b04f
Anand Annapur G V <anandannapur@gmail.com>
2024-12-11 15:16:26 +0530

    creating a middleware

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,12 +1,13 @@
 import User from "../models/user.model.js";
 import bcrypt from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
 
 // Example usage
 
 
 
-export const signup = async(req,res) =>{
+export const signup = async(req,res,next) =>{
     const{username,email,password} = req.body;
     
     const hashsedPassword=bcrypt.hashSync(password,10);
@@ -16,7 +17,7 @@ export const signup = async(req,res) =>{
         await newUser.save();
         res.status(201).json('message recieved');
     }catch(error){
-        res.status(500).json(error)
+        next(error);
     }
     
 
